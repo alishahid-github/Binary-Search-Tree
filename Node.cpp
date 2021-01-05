806736bb057dc7268081aa385f446d75c68c27e1
Ali Shahid <71405228+alishahid-github@users.noreply.github.com>
2021-01-05 11:45:51 +0500

    Implementaiton of Node header class file for BST

---
+++ Node.cpp
@@ -0,0 +1,59 @@
+//.cpp file of the Node class includes implementaion of funtions
+
+#include<iostream>
+#include"BST.h"
+using namespace std;
+
+
+Node::Node()
+{
+	data = 0;
+	Left = NULL;
+	Right = NULL;
+	Parent = NULL;
+}
+
+Node::Node(int d)
+{
+	data = d;
+	Left = NULL;
+	Right = NULL;
+	Parent = NULL;
+}
+
+void Node::set_data(int d) { 
+	data = d;
+}
+void Node::set_Left(Node* l)
+{
+	Left = l;
+}
+void Node::set_Right(Node* r)
+{
+	Right = r;
+}
+
+void Node::set_Parent(Node* p)
+{
+	Parent = p;
+}
+
+int Node::get_data() 
+{ 
+	return data; 
+}
+
+Node* Node::get_Left()
+{
+	return Left;
+}
+
+Node* Node::get_Right()
+{
+	return Right;
+}
+
+Node* Node::get_Parent()
+{
+	return Parent;
+}
