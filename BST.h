78dea0e1cd1fca7ce975e3df08fbcb8d4b172564
Ali Shahid <71405228+alishahid-github@users.noreply.github.com>
2021-01-08 14:19:33 +0500

    Header File for Binary Search Tree

---
+++ BST.h
@@ -0,0 +1,41 @@
+//Header file for the Linked List with Basic funtions
+
+#include<iostream>
+#include"Node.h"
+#ifndef LIST
+#define LIST
+using namespace std;
+class BST
+{
+private:
+	 Node* Root;
+	 string res = "";
+public:
+	int node_count = 0;
+
+	BST();
+	void insert(int d);		
+	Node* search(int d);
+	void inorder(Node* t);
+	void preorder(Node* t);
+	void postorder(Node* t);
+
+	Node* maximum();
+	Node* minimum();
+
+	Node* Successor(int data);
+	Node* Predecessor(int data);
+
+	void set_root(Node* h);
+	Node* get_root();		
+	
+
+	bool DeleteNode(int d);
+	Node* insertLevelOrder(int arr[], Node* root, int i, int n);
+
+	
+
+
+};
+
+#endif
